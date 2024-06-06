532344056af6e9b515425dc2183a223ef3e59c79
ClarenceG01 <gatama001@gmail.com>
2024-06-06 09:36:29 +0300

    cors changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 9000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "*"],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
