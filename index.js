40c1d82e42b0335b0fbcb65ee3c14361ddb287a4
ClarenceG01 <gatama001@gmail.com>
2025-04-10 18:25:51 +0300

    feat: integrate connect-mongo to persist sessions in MongoDB for production

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const { authenticateSession } = require("./src/middleware/authenticate");
+const MongoStore = require("connect-mongo");
 
 const { userRoute } = require("./src/routes/User");
 const { expenseRoute } = require("./src/routes/Expense");
@@ -24,6 +25,11 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+      collectionName: "sessions",
+      ttl: 24 * 60 * 60, // 1 day
+    }),
     cookie: {
       maxAge: 24 * 60 * 60 * 1000, // 1 day
       httpOnly: true,
